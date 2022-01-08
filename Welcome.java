60367fffe8385123d9cfdbd621b2cdda132f0a69
DespoinaMartaki <92679969+DespoinaMartaki@users.noreply.github.com>
2022-01-08 13:06:43 +0200

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -12,7 +12,7 @@ public class Welcome {
         
         
         System.out.println("Enter number of players (up to 4), or press 'q' to quit: ");
-        char numb1 = input.nextChar();     /* The player inputs the number of players */
+        numb1 = input.nextChar();     /* The player inputs the number of players */
         if (numb1 == 'q') {
             System.exit(0);
         } else {
@@ -20,6 +20,7 @@ public class Welcome {
             while (numb < 1 || numb > 4) {
                 System.out.println("Please try again: ");       /* If the player doesn't input a number between 1 and 4, the game asks for the number of players again */
                 numb = input.nextInt();
+            }
         }
         
         if (numb==1) {      /* If the input is 1, then the SinglePlayer class is called */
@@ -30,12 +31,17 @@ public class Welcome {
     }
     
     public static void level() {
-        System.out.print("Choose difficulty level: ");
+        System.out.print("Choose difficulty level, or press 'q' to quit: ");
         System.out.print("1. Easy, 2. Medium, 3. Hard, 4. Mixed");
-        dif = input.nextInt();      /* The player inputs the difficulty level */ 
-        while (dif != 1 && dif != 2 && dif != 3 && dif != 4) {
-            System.out.print("Please try again: ");     /* If the player doesn't input a number between 1 and 4, the game asks for the difficulty level again */
-            dif = input.nextInt();
+        dif1 = input.nextChar();      /* The player inputs the difficulty level */
+        if (dif1 == 'q') {
+            System.exit(0);
+        } else {
+            int dif = Character.getNumericValue(dif1);        /*We turn the char variable dif1 into an int*/
+            while (dif != 1 && dif != 2 && dif != 3 && dif != 4) {
+                System.out.print("Please try again: ");     /* If the player doesn't input a number between 1 and 4, the game asks for the difficulty level again */
+                dif = input.nextInt();
+            }
         }
     }
 }
