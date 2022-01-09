3cc8dd8d1d903c8be274c6469578b75ca1f280d7
eirianna <95477609+eirianna@users.noreply.github.com>
2022-01-09 03:14:14 +0200

    Update MultiPlayer.java

--- MultiPlayer.java
+++ MultiPlayer.java
@@ -8,23 +8,27 @@ public class MultiPlayer {
     public static void questions() {
 
 
-        if (dif == 1) {
+        if (dif == 1) { //if input is 1 then EasyQuestions class is called
             EasyQuestions.setEasyQuestions();
             QnA = EasyQuestions.randomQuestions();
-        } else if (dif == 2) {
+        } else if (dif == 2) { //if input is 2 then MediumQuestions class is called
             MediumQuestions.setMediumQuestions();
             QnA = MediumQuestions.randomQuestions();
-        } else if (dif == 3) {
+        } else if (dif == 3) { //if input is 3 then HardQuestions class is called
             HardQuestions.setHardQuestions();
             QnA = HardQuestions.randomQuestions();
-        } else {
+        } else { // //if input is 4 then MixedQuestions class is called
             QnA = MixedQuestions.randomQuestions();
         }
-        String answer[] = new String[numb];
-        int points[] = new int[numb];
+        
+        String answer[] = new String[numb]; 
+        
+        //array for the sum of the points of each player
+        int points[] = new int[numb]; 
         for (int k = 0; k < numb; k++) {
             points[k] = 0;
         }
+        
         for (int i = 0; i < QnA.size; i++) {
             System.out.println(QnA[i, 0]);
             for (int j = 0; j < numb; j++) {
@@ -37,7 +41,7 @@ public class MultiPlayer {
 
                 if (answer[j] != "q") { 
                     if (answer[j] == QnA[i,1]){
-                    //points
+                    points[j] += 1; //for each correct answer the player earns 1 point
                     }
                     if (j < numb - 1) {
                         System.out.println("Next player");
@@ -48,5 +52,13 @@ public class MultiPlayer {
             }
             System.out.println("The correct answer is " + QnA[i,1])
         }
-
+        //ranking the points of each player from highest score to lowest
+        // mallon tha prepei na exoume k onomata h player#1 kapoia onomasia se pinaka oste na tin taksinomisoume k aftin kai na einai adistixi
+        // me tous pontous oste na ine ksekatharo poios ine o nikitis
+        for (int j = 0; j < (numb - 1); j++) {
+            if (point[j] < points[j + 1]) {
+                int temp = point[j];
+                point[j] = point[j-1];
+                point[j-1] = temp;
+            }
     }
