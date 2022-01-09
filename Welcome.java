e6c11d3ffb6163a072116191a6aeb5e31bd5f40f
kitzos <95079140+kitzos@users.noreply.github.com>
2022-01-09 18:42:47 +0200

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -12,19 +12,18 @@ public class Welcome {
         
         
         System.out.println("Enter number of players (up to 4), or press 'q' to quit: ");
-        numb1 = input.nextChar();     /* The player inputs the number of players */
+       do {
+           System.out.println("Enter number of players (up to 4), or press 'q' to quit: ");
+            numb1 = input.nextChar();     /* The player inputs the number of players */
+           if (numb1 != '1' AND numb1 != '2' AND numb1 != '3' AND numb1 != 4 AND numb1 != 'q') {
+               System.out.println("Please try again!");
+           }
+        } while (numb1 != '1' AND numb1 != '2' AND numb1 != '3' AND numb1 != 4 AND numb1 != 'q');
+                
+        
         if (numb1 == 'q') {
             System.exit(0);
-        } else {
-            int numb = Character.getNumericValue(numb1);        /*We turn the char variable numb1 into an int*/
-            while (numb < 1 || numb > 4) {
-                System.out.println("Please try again: ");       /* If the player doesn't input a number between 1 and 4, the game asks for the number of players again */
-                numb = input.nextInt();
-            }
-            numb1 = (char)numb;
-        }
-        
-        if (numb==1) {      /* If the input is 1, then the SinglePlayer class is called */
+        } else if (numb==1) {      /* If the input is 1, then the SinglePlayer class is called */
             SinglePlayer.questions();
         } else {
             MultiPlayer.questions(); /* If the input isn't 1, then the MultiPlayer class is called */
@@ -32,18 +31,13 @@ public class Welcome {
     }
     
     public static void level() {
-        System.out.print("Choose difficulty level, or press 'q' to quit: ");
-        System.out.print("1. Easy, 2. Medium, 3. Hard, 4. Mixed");
-        dif1 = input.nextChar();      /* The player inputs the difficulty level */
-        if (dif1 == 'q') {
-            System.exit(0);
-        } else {
-            int dif = Character.getNumericValue(dif1);        /*We turn the char variable dif1 into an int*/
-            while (dif != 1 && dif != 2 && dif != 3 && dif != 4) {
-                System.out.print("Please try again: ");     /* If the player doesn't input a number between 1 and 4, the game asks for the difficulty level again */
-                dif = input.nextInt();
+        do {
+            System.out.print("Choose difficulty level, or press 'q' to quit: ");
+            System.out.print("1. Easy, 2. Medium, 3. Hard, 4. Mixed");
+            dif1 = input.nextChar();      /* The player inputs the difficulty level */
+            if (dif1 != '1' AND dif1 != '2' AND dif1 != '3' AND dif1 != 4 AND dif1 != 'q') {
+                System.out.println("Please try again!");
             }
-            dif1 = (char)dif;
-        }
-    }
+        } while (dif1 != '1' AND dif1 != '2' AND dif1 != '3' AND dif1 != 4 AND dif1 != 'q');
+   }
 }
