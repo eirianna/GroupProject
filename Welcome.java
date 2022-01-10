5647bbcba833794d1e62d7e98f301a62925684ed
zoepoulopoulou <95698140+zoepoulopoulou@users.noreply.github.com>
2022-01-10 17:52:04 +0200

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,43 +1,43 @@
 import java.util.Scanner;
 
 public class Welcome {
-    
+
     public static void message() {
         System.out.println("Welcome to the game!");
     }
-    
+
     static Scanner input = new Scanner(System.in);
-    
+
     public static void players() {
-        
-        
+
+
         System.out.println("Enter number of players (up to 4), or press 'q' to quit: ");
-       do {
-           System.out.println("Enter number of players (up to 4), or press 'q' to quit: ");
-            numb1 = input.nextChar();     /* The player inputs the number of players */
-           if (numb1 != '1' AND numb1 != '2' AND numb1 != '3' AND numb1 != 4 AND numb1 != 'q') {
-               System.out.println("Please try again!");
-           }
-        } while (numb1 != '1' AND numb1 != '2' AND numb1 != '3' AND numb1 != 4 AND numb1 != 'q');
-                
-        
+        do {
+            System.out.println("Enter number of players (up to 4), or press 'q' to quit: ");
+            numb1 = input.next();     /* The player inputs the number of players */
+            if (numb1 != '1' && numb1 != '2' && numb1 != '3' && numb1 != 4 && numb1 != 'q') {
+                System.out.println("Please try again!");
+            }
+        } while (numb1 != '1' && numb1 != '2' && numb1 != '3' && numb1 != 4 && numb1 != 'q');
+
+
         if (numb1 == 'q') {
             System.exit(0);
-        } else if (numb==1) {      /* If the input is 1, then the SinglePlayer class is called */
+        } else if (numb1 == 1) {      /* If the input is 1, then the SinglePlayer class is called */
             SinglePlayer.questions();
         } else {
             MultiPlayer.questions(); /* If the input isn't 1, then the MultiPlayer class is called */
         }
     }
-    
+
     public static void level() {
         do {
             System.out.print("Choose difficulty level, or press 'q' to quit: ");
             System.out.print("1. Easy, 2. Medium, 3. Hard, 4. Mixed");
-            dif1 = input.nextChar();      /* The player inputs the difficulty level */
-            if (dif1 != '1' AND dif1 != '2' AND dif1 != '3' AND dif1 != 4 AND dif1 != 'q') {
+            dif1 = input.next();      /* The player inputs the difficulty level */
+            if (dif1 != '1' && dif1 != '2' && dif1 != '3' && dif1 != 4 && dif1 != 'q') {
                 System.out.println("Please try again!");
             }
-        } while (dif1 != '1' AND dif1 != '2' AND dif1 != '3' AND dif1 != 4 AND dif1 != 'q');
-   }
+        } while (dif1 != '1' && dif1 != '2' && dif1 != '3' && dif1 != 4 && dif1 != 'q');
+    }
 }
