424b7fa06622d3e36e941112494eb5a0ea9092a4
kitzos <95079140+kitzos@users.noreply.github.com>
2022-01-11 13:08:40 +0200

    Update SinglePlayer.java

--- SinglePlayer.java
+++ SinglePlayer.java
@@ -3,26 +3,27 @@ import java.util.ArrayList;
 
 public class SinglePlayer {
     
-    ArrayList<ArrayList<String>> QnA = new ArrayList<>();
-    
     public static void questions() {
         
         int dif = Character.getNumericValue(dif1);
-        if (dif == 1){               /* If the input is 1, the EasyQuestions class is called */
-            EasyQuestions.setEasyQuestions();
-            QnA = EasyQuestions.randomQuestions();
-        } else if (dif == 2){       /* If the input is 2, the EasyQuestions class is called */
-            MediumQuestions.setMediumQuestions();
-            QnA = MediumQuestions.randomQuestions();
-        } else if (dif == 3){       /* If the input is 3, the EasyQuestions class is called */
-            HardQuestions.setHardQuestions();
-            QnA = HardQuestions.randomQuestions();
+        if (dif != 4) {
+            String[][] QnA = new String[10][2];
+            if (dif == 1){               /* If the input is 1, the EasyQuestions class is called */
+                EasyQuestions.setEasyQuestions();
+                QnA = EasyQuestions.randomQuestions();
+            } else if (dif == 2){       /* If the input is 2, the EasyQuestions class is called */
+                MediumQuestions.setMediumQuestions();
+                QnA = MediumQuestions.randomQuestions();
+            } else {       /* If the input is 3, the EasyQuestions class is called */
+                HardQuestions.setHardQuestions();
+                QnA = HardQuestions.randomQuestions();
         } else {                    /* If the input is 4 , the MixedQuestions class is called */
+            String[][] QnA = new String[20][2];    
             QnA = MixedQuestions.randomQuestions();
         }
         int points = 0;
         String answer;
-        for (int i=0 ; i<QnA.size ; i++) {
+        for (int i=0 ; i<QnA.length ; i++) {
             System.out.println(QnA[i,0]);
             do {
                 answer = sc.nextString();
@@ -31,16 +32,16 @@ public class SinglePlayer {
                 }
             } while (answer != "1" && answer != "2" && answer != "3" && answer != "4" && answer != "q")
             
-            if (answer == QnA[i,1]) {
+            if (answer == "q") {
+                System.exit(0);     /* This is the case where the player has chosen q, so the game exits */
+            } else if (answer == QnA[i,1]) {
                 System.out.println("CORRECT ANSWER!");
                 points = points + 100;
-            } else if (answer <> QnA[i,1]) {
-                System.out.println("WRONG ANSWER...");
             } else {
-                System.exit(0);     /* This is the case where the player has chosen q, so the game exits */
+                System.out.println("WRONG ANSWER...");
             }
         }
-        int realPoints = points / QnA.size;     /* realPoints = the real value of total points based on the number of the questions the player has been asked*/
+        int realPoints = points / QnA.length;     /* realPoints = the real value of total points based on the number of the questions the player has been asked*/
         System.out.println("The game has ended, you gathered %d points!", points);
             if (realPoints <50) {
                 System.out.println("You can do better than that!");
