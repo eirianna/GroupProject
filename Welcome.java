cff42037e0c0d984c2c1b42155042e5c2dfd5e04
panagiotispetrop <92953373+panagiotispetrop@users.noreply.github.com>
2022-01-14 21:40:01 +0200

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Welcome {
 
     public static void message() {
-        System.out.println("Welcome to the game!");
+        System.out.println("Καλώς ήρθατε στο παιχνίδι!");
     }
 
     static Scanner input = new Scanner(System.in);
@@ -11,12 +11,12 @@ public class Welcome {
     public static void players() {
 
 
-        System.out.println("Enter number of players (up to 4), or press 'q' to quit: ");
+        System.out.println("Εισάγετε αριθμό παικτών (μέχρι 4), ή πατήστε 'q' για να βγείτε: ");
         do {
-            System.out.println("Enter number of players (up to 4), or press 'q' to quit: ");
+            System.out.println("Εισάγετε αριθμό παικτών (μέχρι 4), ή πατήστε 'q' για να βγείτε: ");
             numb1 = input.next().charAt(0);     /* The player inputs the number of players */
             if (numb1 != '1' && numb1 != '2' && numb1 != '3' && numb1 != 4 && numb1 != 'q') {
-                System.out.println("Please try again!");
+                System.out.println("Παρακαλώ προσπαθείστε ξανά!");
             }
         } while (numb1 != '1' && numb1 != '2' && numb1 != '3' && numb1 != 4 && numb1 != 'q');
 
@@ -32,11 +32,11 @@ public class Welcome {
 
     public static void level() {
         do {
-            System.out.print("Choose difficulty level, or press 'q' to quit: ");
-            System.out.print("1. Easy, 2. Medium, 3. Hard, 4. Mixed");
+            System.out.print("Επιλέξτε επίπεδο δυσκολίας, ή πατήστε 'q' για να βγείτε: ");
+            System.out.print("1. Εύκολο, 2. Μέτριο, 3. Δύσκολο, 4. Μίξη");
             dif1 = input.next().charAt(0);      /* The player inputs the difficulty level */
             if (dif1 != '1' && dif1 != '2' && dif1 != '3' && dif1 != 4 && dif1 != 'q') {
-                System.out.println("Please try again!");
+                System.out.println("Παρακαλώ προσπαθείστε ξανά!");
             }
         } while (dif1 != '1' && dif1 != '2' && dif1 != '3' && dif1 != 4 && dif1 != 'q');
     }
