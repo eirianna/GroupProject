c3f3d45803946be5b320ce54d62068be5742ec6b
zoepoulopoulou <95698140+zoepoulopoulou@users.noreply.github.com>
2022-01-14 20:15:56 +0200

    Update MixedQuestions.java

--- MixedQuestions.java
+++ MixedQuestions.java
@@ -1,33 +1,33 @@
 import java.util.Random;
 
 public class MixedQuestions {
-    
-    
-    
-    public static String randomQuestions() {
-        
+
+
+
+    public static String[][] randomQuestions() {
+
         String [] mixedQE = new String[60];             //includes all the questions of all levels
         String [] mixedAN = new String[60];             //includes all the answers of all levels
-        
+
         EasyQuestions.setEasyQuestions();                 //setting every level's questions
         MediumQuestions.setMediumQuestions();
         HardQuestions.setHardQuestions();
-        mixedQE = EasyQuestions.getEasyQuestions;           //filling mixedQE with the questions 
-        mixedQE = MediumQuestions.getMediumQuestions;
-        mixedQE = HardQuestions.getHardQuestions;
-        
+        mixedQE = EasyQuestions.getEasyQuestions();           //filling mixedQE with the questions
+        mixedQE = MediumQuestions.getMediumQuestions();
+        mixedQE = HardQuestions.getHardQuestions();
+
         EasyQuestions.setEasyAnswers();                       //same for answers
         MediumQuestions.setMediumAnswers();
         HardQuestions.setHardAnswers();
-        mixedAN = EasyQuestions.getEasyAnswers;
-        mixedAN = MediumQuestions.getMediumAnswers;
-        mixedAN = HardQuestions.getHardAnswers;
-        
+        mixedAN = EasyQuestions.getEasyAnswers();
+        mixedAN = MediumQuestions.getMediumAnswers();
+        mixedAN = HardQuestions.getHardAnswers();
+
         Random randomNumber = new Random();                 //creation of array with the 20 random questions and answers
         int cn;
         String[][] mixedArray = new String[20][2];
         int[] controlArray = new int[60];
-        
+
         for (int i = 0; i < 60; i++) {
             controlArray[i] = -1;
         }
@@ -38,11 +38,11 @@ public class MixedQuestions {
             } while (controlArray[cn] == cn);
 
             controlArray[cn] = cn;
-            String cq = mixedQE[cn]; //cq = chosen question 
+            String cq = mixedQE[cn]; //cq = chosen question
             String ra = mixedAN[cn]; //ra = right answer
             mixedArray[i][0] = cq;
             mixedArray[i][1] = ra;
         }
-        return mixedArray[20][2];
+        return mixedArray;
     }
 }
