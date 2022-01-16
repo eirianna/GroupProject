020074cc2f0ff6da2430fa44b9b45f18ecb29827
DespoinaMartaki <92679969+DespoinaMartaki@users.noreply.github.com>
2022-01-16 17:53:11 +0200

    Update SinglePlayer.java

--- SinglePlayer.java
+++ SinglePlayer.java
@@ -12,37 +12,41 @@ public class SinglePlayer {
                 EasyQuestions.setEasyQuestions();
                 QnA = EasyQuestions.randomQuestions();
             } else if (dif == 2) {       /* If the input is 2, the EasyQuestions class is called */
+
                 MediumQuestions.setMediumQuestions();
                 QnA = MediumQuestions.randomQuestions();
             } else if (dif == 3) {       /* If the input is 3, the EasyQuestions class is called */
+
                 HardQuestions.setHardQuestions();
                 QnA = HardQuestions.randomQuestions();
             }
         } else {                    /* If the input is 4 , the MixedQuestions class is called */
             QnA = new String[20][2];
+
             QnA = MixedQuestions.randomQuestions();
         }
         int points = 0;
         String answer;
-        
+
         Scanner sc = new Scanner(System.in);
-        
+
         for (int i = 0; i < QnA.length; i++) {
             System.out.println(QnA[i][0]);
             do {
                 answer = sc.nextLine();
-                if (answer != "1" && answer != "2" && answer != "3" && answer != "4" && answer != "q") {       /* If answer is not one of these options, the game asks the player asks the player to answer again */
+                if (!answer.equals( "1") && !answer.equals( "2") && !answer.equals( "3") && !answer.equals( "4") && !answer.equals( "q")) {       /* If answer is not one of these options, the game asks the player asks the player to answer again */
                     System.out.println("Λάθος επιλογή. Παρακαλώ προσπαθείστε ξανά.");
                 }
-            } while (answer != "1" && answer != "2" && answer != "3" && answer != "4" && answer != "q");
+            } while (!answer.equals( "1") && !answer.equals( "2") && !answer.equals( "3") && !answer.equals( "4") && !answer.equals( "q"));
 
-            if (answer == "q") {
+            if (answer.equals( "q")) {
                 System.exit(0);     /* This is the case where the player has chosen q, so the game exits */
-            } else if (answer == QnA[i][1]) {
+            } else if (answer.equals(QnA[i][1])) {
                 System.out.println("ΣΩΣΤΉ ΑΠΆΝΤΗΣΗ!");
                 points = points + 100;
             } else {
                 System.out.println("ΛΆΘΟΣ ΑΠΆΝΤΗΣΗ...");
+                /*System.out.printf("Η σωστή απάντηση είναι: ", QnA[i][1]);*/
             }
         }
         int realPoints = points / QnA.length;     /* realPoints = the real value of total points based on the number of the questions the player has been asked*/
@@ -52,9 +56,10 @@ public class SinglePlayer {
         } else if (realPoints < 80) {
             System.out.println("Έχεις κάνει την έρευνα σου!!!");
         } else {
-            System.out.println("Συγχαρητήρια! Οι γνώσεις σου ειναι αξιοθαύμαστες");
+            System.out.println("Μήπως είσαι από την Ελλάδα;");
         }
         System.out.println("Η ομάδα προγραμματιστών σε ευχαριστεί που ήσουν μαζί μασ σε αυτό το εκπαιδευτικό ταξίδι.");
         System.out.println("Ελπίζουμε να το απόλαυσες όσο και εμείς!");
     }
 }
+
